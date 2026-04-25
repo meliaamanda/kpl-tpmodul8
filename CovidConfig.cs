b4077879fc8a794227435314f32842048e3b979c
Amelia amanda <amelia_amanda_30rpl@student.smktelkom-mlg.sch.id>
2026-04-25 15:29:54 +0700

    first commit

---
+++ CovidConfig.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace tpmodul8_103082400039
+{
+    internal class CovidConfig
+    {
+        public string satuan_suhu { get; set; }
+        public int batas_hari_deman { get; set; }
+        public string pesan_ditolak { get; set; }
+        public string pesan_diterima { get; set; }
+
+        private const string filePath = "covid_config.json";
+
+        public CovidConfig()
+        {
+            //LoadConfig();
+        }
+
+        public void LoadConfig()
+        {
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                CovidConfig data = JsonSerializer.Deserialize<CovidConfig>(json);
+
+                satuan_suhu = data.satuan_suhu;
+                batas_hari_deman = data.batas_hari_deman;
+                pesan_ditolak = data.pesan_ditolak;
+                pesan_diterima = data.pesan_diterima;
+            }
+            else
+            {
+                satuan_suhu = "celcius";
+                batas_hari_deman = 14;
+                pesan_ditolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+                pesan_diterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+                SaveConfig();
+            }
+        }
+
+        public void SaveConfig()
+        {
+            string json = JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(filePath, json);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using tpmodul8_103082400039;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CovidConfig config = new CovidConfig();
+        config.LoadConfig();
+
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {config.satuan_suhu}");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+        int hari = Convert.ToInt32(Console.ReadLine());
+
+        bool suhuValid = false;
+
+        if (config.satuan_suhu == "celcius")
+        {
+            suhuValid = (suhu >= 36.5 && suhu <= 37.5);
+        }
+        else
+        {
+            suhuValid = (suhu >= 97.7 && suhu <= 99.5);
+        }
+
+        bool hariValid = (hari < config.batas_hari_deman);
+
+        if (suhuValid && hariValid)
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.pesan_ditolak);
+        }
+    }
+}
